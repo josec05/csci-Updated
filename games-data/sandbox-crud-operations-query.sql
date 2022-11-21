58bde97d64dc20acc7d4ab2cf631e7109f843b4f
josec05 <jose.cuevas05@utrgv.edu>
2022-11-21 13:31:23 -0600

    ADD games data project

---
+++ games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+SELECT * FROM sandbox;
+
+--INSERT INTO sandbox(int32_value,bool_value,double_value) VALUES (731937465,0,898478.9395);
+
+--UPDATE sandbox SET int64_value = 21548 WHERE id = 25;
+
+--DELETE FROM sandbox WHERE id = 36;
